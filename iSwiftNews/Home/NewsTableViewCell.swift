aeb7caa762928aea31d9fd5e782338fc25614693
Fikri Bill Gufran <billgufran@gmail.com>
2023-05-04 16:10:37 +0800

    feat: create NewsTableViewCell

---
+++ iSwiftNews/Home/NewsTableViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  NewsTableViewCell.swift
+//  iSwiftNews
+//
+//  Created by Muhammad Fikri Bill Gufran on 4/5/23.
+//
+
+import UIKit
+
+protocol NewsTableViewCellDelegate: AnyObject {
+    func cellBookmarkButtonTapped(_ cell: NewsTableViewCell)
+}
+
+class NewsTableViewCell: UITableViewCell {
+    @IBOutlet weak var thumbImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var bookmarkButton: UIButton!
+    
+    weak var delegate: NewsTableViewCellDelegate?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        setup()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setup() {
+        setThumbImageStyles()
+    }
+    
+    func setThumbImageStyles() {
+        thumbImage.layer.cornerRadius = 8
+        thumbImage.layer.masksToBounds = true
+    }
+    
+    @IBAction func bookmarkButtonTapped(_ sender: Any) {
+        delegate?.cellBookmarkButtonTapped(self)
+    }
+}
