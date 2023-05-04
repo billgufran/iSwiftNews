3648beb4c636e391f30b68208812590b3b4b1e01
Fikri Bill Gufran <billgufran@gmail.com>
2023-05-04 20:57:34 +0800

    feat: init loadNews in HomeViewController

--- iSwiftNews/Home/HomeViewController.swift
+++ iSwiftNews/Home/HomeViewController.swift
@@ -8,22 +8,28 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    var newsList: [News] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        loadNews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func loadNews() {
+        ApiService.shared.loadNews { [weak self] result in
+            guard let `self` = self else { return }
+            
+            switch result {
+            case .success(let newsList):
+                //@todo remove print
+                print(newsList)
+                self.newsList = newsList
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
-    */
-
 }
